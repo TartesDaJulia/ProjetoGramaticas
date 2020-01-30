14d7db418967591f8ae833e28070d402ca66abf3
Henrique Ribeiro <pg38415@alunos.uminho.pt>
2020-01-30 21:38:02 +0000

    Listens for files and Creates new json file

--- src/ISNCVVisitor.java
+++ src/ISNCVVisitor.java
@@ -1,11 +1,18 @@
+import sun.reflect.annotation.ExceptionProxy;
+
 import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+
 public class ISNCVVisitor extends  ISNBaseVisitor<String>{
 
     public List<String> data = new ArrayList<String>();
     public List< List<String> >registers = new ArrayList< List<String>>();
-
+    public StringBuilder parsed = new StringBuilder();
 
     public ISNCVVisitor() {
+
     }
 
     @Override
@@ -14,10 +21,40 @@ public class ISNCVVisitor extends  ISNBaseVisitor<String>{
         {
             visit(reg);
         }
-        System.out.println("Vou imprimir a lista conseguida...\n");
+        System.out.println("Vou imprimir a lista conseguida...");
+        parsed.append("{ \"registers\": [ ");
         for (int i = 0; i < registers.size(); i++) {
             System.out.println(registers.get(i));
+            parsed.append(" { \"name\" :  "          + registers.get(i).get(0) +"," +
+                            "\"username\" : "       + registers.get(i).get(1) +"," +
+                            "\"mail\" : "           + registers.get(i).get(2) +"," +
+                            "\"dateOfBirth\" : \""  + registers.get(i).get(3) +"\"," +
+                            "\"password\" : "       + registers.get(i).get(4) +"," +
+                            "\"course\" : "         + registers.get(i).get(5) +"," +
+                            "\"type\" : "           + registers.get(i).get(6) +" }"
+                    );
+            if(i!=registers.size()-1){
+                parsed.append(",");
+            }
+            else {
+                parsed.append("]}");
+            }
         }
+        try {
+            File file = new File("C:\\Users\\Henrique\\UMinho\\1Ano2019\\1Semestre\\PRI\\TPC\\PRI2019\\Projeto\\API\\parsedFile\\registers.json");
+            if(file.createNewFile()){
+                try{
+                    FileWriter write = new FileWriter("C:\\Users\\Henrique\\UMinho\\1Ano2019\\1Semestre\\PRI\\TPC\\PRI2019\\Projeto\\API\\parsedFile\\registers.json");
+                    write.append(parsed);
+                    write.close();
+                }catch(Exception e) {
+                    System.out.println(e);
+                }
+            }
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
         return null;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,49 @@ import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.ParserRuleContext;
-
+import java.io.File;
 import java.io.IOException;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
 
     public  static  void main (String[] args) throws IOException{
 
         ISNLexer lexer;
-        lexer = new ISNLexer(CharStreams.fromFileName("C:\\Users\\Henrique\\UMinho\\1Ano2019\\1Semestre\\Gramaticas\\ProjetoGramaticas\\testes\\1.txt"));
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        ISNParser parser = new ISNParser(tokens);
-        ParserRuleContext ctx = parser.registers();
+        boolean control=false;
+        File file = new File("C:\\Users\\Henrique\\UMinho\\1Ano2019\\1Semestre\\PRI\\TPC\\PRI2019\\Projeto\\API\\fileToParse\\registers.txt");
 
-        ISNCVVisitor visitor = new ISNCVVisitor();
-        visitor.visit(ctx);
+        while(!control)
+        {
+            if(file.exists()){
+                try {
+                    lexer = new ISNLexer(CharStreams.fromFileName("C:\\Users\\Henrique\\UMinho\\1Ano2019\\1Semestre\\PRI\\TPC\\PRI2019\\Projeto\\API\\fileToParse\\registers.txt"));
+                    CommonTokenStream tokens = new CommonTokenStream(lexer);
+                    ISNParser parser = new ISNParser(tokens);
+                    ParserRuleContext ctx = parser.registers();
+                    ISNCVVisitor visitor = new ISNCVVisitor();
+                    visitor.visit(ctx);
+                    try{
+                        file.delete();
+                    }
+                    catch(Exception e){
+                        System.out.println(e);
+                    }
+                }catch(Exception e){
+                    System.out.println(e);
+                }
+            }
+            else{
+                try{
+                    Thread.sleep(1000);
+                }catch (Exception e){
+                    System.out.println(e);
+                }
+            }
+        }
+
+
 
 
     }
